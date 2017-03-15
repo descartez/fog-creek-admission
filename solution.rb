69f9677fe4aa9018e8d9d5c89dec06dc94fc9ffb
Ryan Stong <rvanstong@gmail.com>
2017-03-15 15:25:24 -0700

    writes some tests;

--- solution.rb
+++ solution.rb
@@ -6,23 +6,67 @@
 
 big_string = "hayjfltwmnlstaddvdkabpggxpcbmykiymbyphllzozvwjlkqrmaoxnsxqxjhvyrjykaddsoedcyitmevubhgelbmrtk_kxdizfgagidqvbvqrkhwscpenkdqatrcrhbucwmsv_qbygnjsydpr_mqftjviinseckyllydrbmsmyjaszwkdhnpjtk_awdokmpx_fzekzkttovqlfkqvwjbexvzwlnzaqwg_pqlorsmb_mavbuegnsvxptvbgcvairgdgsccac_ug_hbnkbu_svcbrjewiloukndywehdopjckulwwidrycoxsrpngacezescjtcutprmybxdivulcn_znajvbauio_jyahxxnyeyyznkoosgucjlzyqsptpbucirirywbpryjpktw_vl_unycyosq__alklnzv_qkpslqckzmuqbqfjiztsmskqdyxshhxkslvwx_bhaa_opivrqiandjfeyvowrnyysfaem_cgcvxuohpo__umqvbuqnrtcyijtrqsrfjpglsenwypljqixyjvkhogujuljtlowqghlwoonctrkcfwbzqqtqtmzef_elmbybryfiego_jknatrcrtecihdjsjzsytgpcomxkyduygnivcnq_gftnoc_tktdzuubehdjagd_caudrtgujkjzmhxdpvqsg_wyemsrvsdweyclxyhpqeeaphsyhahbbguogsuxsgtzaxktglhxg_xzeqjgmzhpmwfzwpufnrvdnkoyyrdqiwvxflndssbmanersenhltqtmvcmexdusczopalyxgqmopkedsvoszbu_sefquucivicerrnigkutzdojtnqtrfmocyjqs_b_eiqoncrudjccjnwhkjmmaanjsvkycryhxyyhimiebzoukiffcabhlujcwzfnhzlnhdvxyfxarcsqqjwgtbcugqftxrtqrspmpzczbrxpmrlqjqvqaylgvfhxezasdfrarlfxfrbofetptkljmztwvpwrswhmxhsny_bmlkxvyp_pendkcovvsfgkpjirrt_yhue_tgvkggz_shj_aldhfrvcfqxktkxyafaltwqzxoliocjitcikomih_mgygwvxvupgstjpfzbajdmjulbq_trsorfhfpulxnvdeoavcradzphqlngfi_on_kvllspucpxdamnws_ewgxalrlajoeaxxqqondjygfvqfa_ieuuemlozh_qyktxpypmmgou_izoroouiisnpsquplvapnqrmrecjilm_esnsz_steohcisskjioezirv_wlmoipuevrtwthybtrjqpuhuoacmnakhbikccacxb_eicdzwgwfmcdqet_fjum_zkbcokewropykow_cmfkpwuagxgxyfohejmxasb_lzxyzax_bgzjxsb_ollvzqwpyyndezpitwzdeeqthmrhqrhpzwxvdvliycsjiueoxrevkx_gjtpiowhpnit_rdwkocfxvivkfqkom_tjoimirz_ypyufouphocswbzfdprbiirfntvxtvytxgzwjaysxhptbenatxiaxwutrmiwbifqmpkwpmzmlkfzqnprskfsuvnphrbxysufcckssxwcbvapxbkfmum_meucimvshsawiucmimicnwnptynlghaeaaxtopadocumadecakteiw_ckczsvctsjwbaboqkzq_eouxfaiyexmf_iyrdjrkiuuudseoiuitxiyzwctiwgqwokiygfjzdbtimbpgfwypvtwkrgmcevfhqsekybgylzdvohlkwozsvxtkscuejhfjiwcummauh_lufqovxdwamlzfbr_uuquguwyplombdncgwaujpt_zsjziueozsblsxbjhgvzkuitdxaaxoxzm_akqgkulphkxdy_ttwrkxc_vmzqjelfiytessl_dxqitnlrocrmrmdcbudfxehd_wvxkjarnizentw_zoghv_iqjjgssfxeb_gbcu_ewymrelnpmpkteoibldnp_mtygdgauicswadrkagqeglugfa_fgv_oadajyqlsjjunvd_elotxznhrscujaljcqmm_ltlgdhazmzmppniqrs_ovmrioeouyrcuooso_mcuhfqvhtj_eoiomqsstpfrcmhxrrohkgyrdgeaamcbpghuduanwkuafuxbrvmpczqmwrzlur_fslnqbiew_nluucoyyn_yxviekfzmki_lqnijumpdbyptuvthgcvpkqshcs_abxwndxssacqxvbgcunoouhtlnkhlfefztckwntbomtralhdfvatdpyvojwgfjtrtwgwpdgbkh_wauyeda_aopit"
 
-big_hash = Hash.new(0)
+module Decoder
+  def self.decode(big_string)
+    @big_string_array = big_string.split("")
+    @big_hash = Hash.new(0)
+    @big_hash_array = []
+    make_hash
+    array_hash
+    serve_string
+  end
 
-big_string.split("").each do |w|
-  big_hash[w] += 1
-end
+  def self.make_hash
+    @big_string_array.each {|w| @big_hash[w] += 1}
+  end
 
-p secret_hash_arr = big_hash.sort_by{|k,v| v}.reverse
+  def self.array_hash
+    @big_hash_array = @big_hash.sort_by{|k,v| v}.reverse
+  end
 
-secret_word = []
-
-secret_hash_arr.each do |w|
-  p w
-  if w[0] != "_"
-    secret_word << w[0]
-  else
-    break
+  def self.serve_string
+    secret_word = []
+    @big_hash_array.each do |w|
+      if w[0] != "_"
+        secret_word << w[0]
+      else
+        break
+      end
+    end
+    return secret_word.join("")
   end
 end
 
-p secret_word.join("")
+# --------------------------------
+#           TESTS
+# --------------------------------
+
+def test(test_number, test_str, desired)
+  puts "-"*15
+  puts "TEST #{test_number}"
+  puts "-"*15
+  puts "test string: #{test_str}"
+  puts "desired: '#{desired}'"
+  decoded_string = Decoder.decode(test_str)
+  puts "output: '#{decoded_string}'"
+  puts "Passed? #{decoded_string == desired}"
+  puts "\n"
+end
+
+test_str_1 = "aaiiiaallll_laaaliieeenn"
+test(1,test_str_1,"alien")
+
+test_str_2 = "_bbbbbbaaaaarrrrooonn"
+test(2,test_str_2,"baron")
+
+puts "-*-"*5
+puts "Now for all the marbles"
+puts "-*-"*5
+sleep(1)
+puts "-*-"*5
+puts "Drumroll please"
+puts "-*-"*5
+sleep(1)
+puts "The secret_word is..."
+sleep(1)
+puts Decoder.decode(big_string)
